919f9bfd9f5643c4048f279ae35ceb3784be1bbf
Anna <yavianbo@gmail.com>
2024-01-29 21:05:01 +0300

    feat: File system (src/fs)/create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { writeFile } from 'fs/promises';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+
+const currentFileUrl = import.meta.url;
+const currentDirectory = join(fileURLToPath(new URL('.', currentFileUrl)), 'files', 'fresh.txt');
+
 const create = async () => {
-    // Write your code here 
+    try {
+        await writeFile(currentDirectory, 'I am fresh and young', { flag: 'wx' });
+        console.log('New file fresh.txt');
+      
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await create();
